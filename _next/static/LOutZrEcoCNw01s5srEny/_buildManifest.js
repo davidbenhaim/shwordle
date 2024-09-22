178b766b5001d2718bbb36e91f48c892960db3cd
David Benhaim <david@backflip.ai>
2024-09-21 19:38:59 -0700

    Updates

--- _next/static/LOutZrEcoCNw01s5srEny/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/478-b112be8772f9c155.js","static/chunks/pages/index-ef5d7b77aff855f8.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

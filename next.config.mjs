90dcd8cd9915c4e2867493b381eda465b125bdc6
David Benhaim <david@backflip.ai>
2024-09-21 18:40:52 -0700

    Prepare for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/shwordle' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/shwordle' : '',
+}
 
 export default nextConfig;
